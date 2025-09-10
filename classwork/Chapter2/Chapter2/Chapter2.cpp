798b93cba3e2187bc4f7670e939aac59aae0c0f3
Juniper27 <grace.barkley@icloud.com>
2025-09-09 21:20:38 -0500

    casing ans const

--- classwork/Chapter2/Chapter2/Chapter2.cpp
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -5,6 +5,7 @@
 */
 //preprocessor directive 
 #include <iostream>
+#include <string> // needed for string data type
 
 int main()
 {
@@ -95,9 +96,65 @@ int main()
     bool ispassing;
     ispassing = true; // false 
 
+    area = length * width; 
+
     // assignment is right associative (evaliate right to left) 
     //length = 100;
     //width = 100; 
     length = width = 100;
+    //std::cout << length; // DONT USE THIS IS WRONG WAY TO DEBUG
+    // F9 TOGGLE BREAKPOINT pauses the program if there is an issue and let me see (it tells the dubugger to stop here and let me look at the variables)
+    // f5 toggle for debugger to run
+    // yellow error tells you what line of code will run next when started up again
+    // f10 stepover run the code to the next avalible code 
+    
+    //int and Int are different
+    // camel casing > all lowercase, capitalize on word boundaries (firstName) 2nd word capitalized but no spaces
+
+    // arithmetic operators
+    // arith_op ::= + - * / %
+    length = length + 10;
+    width = width - 20;
+
+    // int division :: = int / int produces an int, remainder is lost
+    double result = 10 / 3; // = 3 nearist whole number 
+    
+    // modulus is remainder and ONLY works for the integral types (int, short, long)
+    double remainder = 10 % 3; // 1
+    // int + int = int types must match on both sides of the assignment operator (=) so 10/3 = 3 (whole number) chops off the decimal
+    // remainder operator (%) only works with integers shows the remainder of a division problem since 9+9=18 18/10=1 remainder 8   
+
+    int negativeLength = -length;
+
+    // scope - lifetime of a variable
+    { // starting new block
+        int newVariable1 = 10;
+        double newVariable2 = 45.7;
+    }
+
+    {
+        char newVariable1 = 'A';
+    }
+    //std::cout << newVariable1; // this is out of scope and will give an error
+
+    // strings c++
+    std::string studentName; // compiler cant read string without std:: this is why its not a primitive data type
+    studentName = "June";
+
+    studentName = studentName + " Barkley";
+
+    // literal - great for atatic values that dont change
+    // variable - great for dynamic values that do change (writeable and readable) - grades, emails, names, addresses
+    // Named constant - great for static values that dont change (readable only) - pi, tax rates, conversion factors - cant change them in the program
+    const double pi = 3.14159;
+
+    // A = Pi r^2;
+    double areaCircle = pi * (radius * radius);
+
+    const int NumberOfLabGrades = 5;
+    const int NumberOfExamGrades = 4;
+    // pascal casing > first letter of each word is capitalized (NumberOfLabGrades) common for constants
+
+   // pi = 140; // this will give an error because pi is a constant and cant be changed
 
 }
