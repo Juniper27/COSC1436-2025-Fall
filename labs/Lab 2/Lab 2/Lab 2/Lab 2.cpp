ed5c1c55a18f4d00a5d76726c50302ca2d600e04
Juniper27 <grace.barkley@icloud.com>
2025-10-14 00:11:20 -0500

    lab 2 start

---
+++ labs/Lab 2/Lab 2/Lab 2/Lab 2.cpp
@@ -0,0 +1,49 @@
+// June Barkley
+// Lab 2
+// COSC 1430 Fall 2025
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "Lab 2" << std::endl;
+    std::cout << "June Barkley " << std::endl;
+    std::cout << "COSC 1430 Fall 2025" << std::endl;
+
+    std::cout << "Enter initial loan (Must be between $1 and $1000): ";
+    int initialLoan;
+
+    /*do
+    {
+        std::cin >> initialLoan;
+
+        if (initialLoan < 1 || initialLoan > 1000)
+        {
+            std::string message = "Loan must be between $1 and $1000.";
+            std::cout << "ERROR! " << message << std::endl;
+        }
+      
+    } while (initialLoan < 1 || initialLoan > 1000);
+
+    std::cout << "Enter ititial loan (Must be between $1 and $1000): ";
+    std::cin >> initialLoan;
+    */
+
+   /* do
+    { 
+    std::cin >> initialLoan;
+
+    if (initialLoan < 1 || initialLoan > 1000)
+    {
+        std::string message = "Loan must be between $1 and $1000.";
+        std::cout << "ERROR: " << message << std::endl;
+    }
+
+    } while (initialLoan < 1 || initialLoan > 1000);
+    std::cout << "Enter initial loan (Must be between $1 and $1000): ";
+    std::cin >> initialLoan;
+   */
+
+}
