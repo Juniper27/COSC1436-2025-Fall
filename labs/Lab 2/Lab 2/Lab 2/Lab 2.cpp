dc09d366f3edf5ea1ad1c8c6f0825dab1f2e4a4c
Juniper27 <grace.barkley@icloud.com>
2025-10-14 16:26:00 -0500

    lab 2 progress

--- labs/Lab 2/Lab 2/Lab 2/Lab 2.cpp
+++ labs/Lab 2/Lab 2/Lab 2/Lab 2.cpp
@@ -12,9 +12,21 @@ int main()
     std::cout << "June Barkley " << std::endl;
     std::cout << "COSC 1430 Fall 2025" << std::endl;
 
-    std::cout << "Enter initial loan (Must be between $1 and $1000): ";
     int initialLoan;
+    do
+    {
+        std::cout << "Enter initial loan (Must be between $1 and $1000): ";
+        std::cin >> initialLoan;
 
+        if (initialLoan < 1 || initialLoan > 1000)
+        {
+            std::string message = "Loan must be between $1 and $1000.";
+            std::cout << "ERROR! " << message << std::endl;
+        }
+        
+
+    } while (initialLoan < 1 || initialLoan > 1000);
+    std::cout << "\n";
     /*do
     {
         std::cin >> initialLoan;
@@ -25,25 +37,26 @@ int main()
             std::cout << "ERROR! " << message << std::endl;
         }
       
-    } while (initialLoan < 1 || initialLoan > 1000);
-
-    std::cout << "Enter ititial loan (Must be between $1 and $1000): ";
-    std::cin >> initialLoan;
-    */
-
-   /* do
-    { 
-    std::cin >> initialLoan;
-
-    if (initialLoan < 1 || initialLoan > 1000)
+    } while (initialLoan < 1 || initialLoan > 1000); */
+    float interestRate;
+    do
     {
-        std::string message = "Loan must be between $1 and $1000.";
-        std::cout << "ERROR: " << message << std::endl;
-    }
+        std::cout << "Enter intrest rate % (Must be between 1.0 and 100.0): ";
+        std::cin >> interestRate;
 
-    } while (initialLoan < 1 || initialLoan > 1000);
-    std::cout << "Enter initial loan (Must be between $1 and $1000): ";
-    std::cin >> initialLoan;
-   */
+        if (interestRate < 1.0 || interestRate > 100.0)
+        {
+            std::string message = "Interest rate % must be between 1.0 and 100.0.";
+            std::cout << "ERROR! " << message << std::endl;
+        }
+    } while (interestRate < 1.0 || interestRate > 100.0);
+    std::cout << "\n";
+
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "First 12 Months of Loan" << std::endl;
+    std::cout << std::setw(50) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+    std::cout << std::setw(9) << std::left << "Month " << std::setw(15) << "Loan Balance " << std::setw(9) << "Intrest " << std::setw(15) << "New Balance " << std::endl;
+    std::cout << std::setw(50) << std::setfill('-') << "" << std::setfill(' ') << std::endl;
+
 
 }
